9636f59bf68e8a4e8b4f5fb81ad701fe9fc60217
angelina <potapochkinaangelina@gmail.com>
2026-05-09 01:54:41 +0300

    fix: correct vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/oge-frontend/',
+  base: "/",
   server: {
     port: 5173,
-    open: true
-  }
-})
+    open: true,
+  },
+});
